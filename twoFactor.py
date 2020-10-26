efdb23dd14b7f314b3e38d2b11546d695125fd16
Drew DeLap <delap.8s@osu.edu>
2020-10-26 15:49:07 -0400

    Python program setup

---
+++ twoFactor.py
@@ -0,0 +1,2 @@
+#Authors: Drew DeLap and Evan Standerwick
+#Purpose: This program should provide Zoom with a 2-factor identification plug-in that allows the user to select their preferred 2-factor software (google vs duo) through a GUI
